245fc8559a517d6ffbd0afac90905cdafadeb89d
sushil sharma <sharma.s420823@gmail.com>
2024-10-11 10:37:34 +0545

    -

---
+++ React/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Front from './assets/components/Front'
+
+const App = () => {
+  return (
+    <div className='font-af w-screen h-screen bg-slate-700 text-white'>
+      
+     
+     <Front/>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ React/src/assets/components/Front.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Front = () => {
+  return (
+    <div>
+        <div className='h-screen flex justify-center items-center'>
+            <div className='input'>
+                <input className='focus:outline-none p-3 text-2xl border-none text-black w-[30vw] h-[2.5vw]' type="text" placeholder='Enter tasks lists' />
+            </div>
+        </div>
+
+
+    </div>
+  )
+}
+
+export default Front
