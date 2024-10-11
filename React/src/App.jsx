2c023c37338789686e15ec56d82fa14405fa2cbe
sushil sharma <sharma.s420823@gmail.com>
2024-10-11 11:48:00 +0545

    -

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Front from './assets/components/Front'
 
 const App = () => {
+  const [inputText, setinputText] = useState("")
   return (
+    
     <div className='font-af w-screen h-screen bg-slate-700 text-white'>
       
-     
-     <Front/>
-      
+     {/* <h1>TO-DO LIST {inputText}</h1> */}
+     <Front setinputText={setinputText}/>
     </div>
   )
 }
--- React/src/assets/components/Front.jsx
+++ React/src/assets/components/Front.jsx
@@ -1,11 +1,42 @@
 import React from 'react'
+import { IoAddSharp } from "react-icons/io5";
 
-const Front = () => {
+
+const Front = ({setinputText}) => {
+
+    const inputData=(e)=>{
+        // console.log(e.target.value)
+        setinputText(e.target.value)
+    }
+
+    const added=()=>{
+        console.log("added")
+    }
+
+
   return (
     <div>
-        <div className='h-screen flex justify-center items-center'>
-            <div className='input'>
-                <input className='focus:outline-none p-3 text-2xl border-none text-black w-[30vw] h-[2.5vw]' type="text" placeholder='Enter tasks lists' />
+        <div className=' h-screen flex gap-2 justify-center items-center'>
+
+            <div className='flex relative'>
+                <div onChange={inputData} className='input'>
+                    <input className=' focus:outline-none px-3 pr-[2.5vw] text-2xl border-none text-black w-[30vw] h-[2.5vw]' type="text" placeholder='Enter tasks lists' />
+                </div>
+                <div onClick={added} className='cursor-pointer text-[1.5vw] top-[50%] right-[0%] -translate-x-[50%] -translate-y-[50%]  absolute text-black'>
+                    <IoAddSharp/>
+                </div>
+            </div>
+
+
+
+
+            <div className='filters text-black'>
+                <select className='cursor-pointer text-xl focus:outline-none w-[10vw] h-[2.5vw]' name="todos" id="">
+                    <option value="default" disabled selected>Filter by</option>
+                    <option value="all">All</option>
+                    <option value="completed">Completed</option>
+                    <option value="remaining">Remaining</option>
+                </select>
             </div>
         </div>
 
